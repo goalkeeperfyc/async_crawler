efc4565967f02a6aad88bb615b4e6f2089049008
goalkeeperfyc <664947387@qq.com>
2018-11-12 19:44:39 +0800

    framework is on the way

---
+++ scrap_list_page_async.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 12 19:03:43 2018
+
+@author: fangyucheng
+"""
+
+import argparse
+import configparser
+from crawler.crawler_sys.framework.platform_crawler_register import get_crawler
+
+arg_parse = argparse.ArgumentParser(description="crawler for video website list page")
+arg_parse.add_argument('-p', '--platform', default='腾讯视频', type=str, help=('a platform must be input'))
+arg_parse.add_argument('-c', '--conf', default='/home/hanye/crawlers/crawler_sys/utils/list_page_conf.ini',
+                       type=str, help=('absolute path of conf file'))
+args = arg_parse.parse_args()
+
+config = configparser.ConfigParser()
+config.read(filenames=)
+platform = args.platform
+
+task_lst = 
